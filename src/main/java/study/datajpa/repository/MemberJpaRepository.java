ae4615756e2308eef17d05b4dc6aabdb41e99a9b
taekgyu <taekgyu0602@gmail.com>
2022-10-19 22:58:48 +0900

    MemberJpaRepository를 테스트함

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -17,7 +19,27 @@ public class MemberJpaRepository {
         return member;
     }
 
-    public Member find(Long id){
+    public void delete(Member member){
+        em.remove(member);
+    }
+
+    public List<Member> findAll(){ // 전체조회
+        // JPQL은 객체를 대상으로 하는 쿼리이다.
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+    public Member find(Long id){ // id로 하나 조회
         return em.find(Member.class, id);
     }
 }
